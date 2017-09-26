f218cf4e390477d8cb1f26979eecff08269bf4b5
rrtigga <rrtigga@ucdavis.edu>
2017-09-26 13:39:10 -0700

    toggle overflow hidden when nav bar is active.

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -1,5 +1,6 @@
 $('#navButton').on('click', function(e) {
   $('#navButton').toggleClass("-active");
   $('#navigation').toggleClass("-on");
+  $('body').toggleClass("overflowHidden");
   e.preventDefault();
 });
